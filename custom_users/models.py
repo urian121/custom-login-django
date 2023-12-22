f59f73dc1f3bf0cc4952222cd759effc1785ebaf
URIAN VIERA <urian1213viera@gmail.com>
2023-12-22 15:09:10 -0500

    iniciando

--- custom_users/models.py
+++ custom_users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20, blank=True)
+    twitter_handle = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.username
